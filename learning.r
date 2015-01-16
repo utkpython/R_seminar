900bdb3e461afe4207fc2069a4bd90d0029e546d
utkpython <utkpython@gmail.com>
2015-01-16 12:09:56 -0500

    Create learning.r

---
+++ learning.r
@@ -0,0 +1,12 @@
+# R is awesome for experimenting with numbers
+# you can use it like a calculator
+# type the following lines in the console window within RStudio
+
+1 + 1
+
+11 / 3
+
+111 / 3 + 3
+
+sqrt(1/3)
+
