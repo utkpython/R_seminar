2408afeb3afbe0ca60f79b5f9f88f4030f04bc96
utkpython <utkpython@gmail.com>
2015-01-16 12:15:26 -0500

    Update learning.r

--- learning.r
+++ learning.r
@@ -13,4 +13,14 @@ sqrt(1/3)
 log(1/3)
 
 
+# you can store numbers so that you don't have to type them again
+# read the following as "my_number" is assigned to be the square root of 111/3
+my_number <- sqrt(111/3)
+
+my_number + 1000
+
+# if you don't like my_number, you can reassign it
+
+my_number <- 7  # after all, 7 is lucky
+
 
